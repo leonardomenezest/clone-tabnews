66b31753c8f857457564182d7b2595934a972fab
Leonardo Menezes <leonardo.fmdl@gmail.com>
2024-11-22 01:39:27 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Izaína, eu te amo. Se você me ama, me dá um beijinho! 😎</h1>
+    return <h1>Izaína, eu te amo. Se você me ama, me dá um beijinho! 😼</h1>
 }
 
 export default Home;
